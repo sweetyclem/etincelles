df516c8c99175c9fab353061004853f6678e8623
uSlam <uSlam@10.10.2.56>
2017-03-13 10:51:10 +0100

    Initial commit (navbar only)

---
+++ Etincelles/src/main/java/com/etincelles/EtincellesApplication.java
@@ -0,0 +1,12 @@
+package com.etincelles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtincellesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtincellesApplication.class, args);
+	}
+}
