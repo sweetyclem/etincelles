0cdac90b8e5eafe3130fa3c14a58c14b7757f168
uSlam <uSlam@10.10.2.51>
2017-03-14 14:54:08 +0100

    Add MailConstructor

--- Etincelles/src/main/java/com/etincelles/controller/HomeController.java
+++ Etincelles/src/main/java/com/etincelles/controller/HomeController.java
@@ -1,29 +1,48 @@
 package com.etincelles.controller;
 
+import java.util.HashSet;
 import java.util.Locale;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.etincelles.entities.PasswordResetToken;
 import com.etincelles.entities.User;
+import com.etincelles.entities.security.Role;
+import com.etincelles.entities.security.UserRole;
 import com.etincelles.service.UserService;
 import com.etincelles.service.impl.UserSecurityService;
+import com.etincelles.utility.MailConstructor;
+import com.etincelles.utility.SecurityUtility;
 
 @Controller
 public class HomeController {
     @Autowired
-    UserService         userService;
+    private JavaMailSender      mailSender;
 
     @Autowired
-    UserSecurityService userSecurityService;
+    private MailConstructor     mailConstructor;
+
+    @Autowired
+    private UserService         userService;
+
+    @Autowired
+    private UserSecurityService userSecurityService;
 
     @RequestMapping( "/" )
     public String index() {
@@ -41,7 +60,57 @@ public class HomeController {
         return "myAccount";
     }
 
-    @RequestMapping( "/newEmail" )
+    @RequestMapping( value = "/newUser", method = RequestMethod.POST )
+    public String newUserPost(
+            HttpServletRequest request,
+            @ModelAttribute( "email" ) String email,
+            Model model ) throws Exception {
+        model.addAttribute( "classActiveNewAccount", true );
+        model.addAttribute( "email", email );
+
+        if ( userService.findByEmail( email ) != null ) {
+            model.addAttribute( "usernameExists", true );
+
+            return "myAccount";
+        }
+
+        if ( userService.findByEmail( email ) != null ) {
+            model.addAttribute( "emailExists", true );
+
+            return "myAccount";
+        }
+
+        User user = new User();
+        user.setEmail( email );
+
+        String password = SecurityUtility.randomPassword();
+
+        String encryptedPassword = SecurityUtility.passwordEncoder().encode( password );
+        user.setPassword( encryptedPassword );
+
+        Role role = new Role();
+        role.setRoleId( 1 );
+        role.setName( "ROLE_USER" );
+        Set<UserRole> userRoles = new HashSet<>();
+        userRoles.add( new UserRole( user, role ) );
+        userService.createUser( user, userRoles );
+
+        String token = UUID.randomUUID().toString();
+        userService.createPasswordResetTokenForUser( user, token );
+
+        String appUrl = "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+
+        SimpleMailMessage mail = mailConstructor.constructResetTokenEmail( appUrl, request.getLocale(), token, user,
+                password );
+
+        mailSender.send( mail );
+
+        model.addAttribute( "emailSent", "true" );
+
+        return "myAccount";
+    }
+
+    @RequestMapping( "/newUser" )
     public String newUser( Locale locale, @RequestParam( "token" ) String token, Model model ) {
         PasswordResetToken passToken = userService.getPasswordResetToken( token );
 
---
+++ Etincelles/src/main/java/com/etincelles/utility/MailConstructor.java
@@ -0,0 +1,31 @@
+package com.etincelles.utility;
+
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+import com.etincelles.entities.User;
+
+@Component
+public class MailConstructor {
+    @Autowired
+    private Environment env;
+
+    public SimpleMailMessage constructResetTokenEmail(
+            String contextPath, Locale locale, String token, User user, String password ) {
+
+        String url = contextPath + "/newUser?token=" + token;
+        String message = "\nVeuillez cliquer sur le lien pour confirmer votre compte. Votre mot de passe est : \n"
+                + password;
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setTo( user.getEmail() );
+        email.setSubject( "Plateforme Etincelles- Créer un compte" );
+        email.setText( url + message );
+        email.setFrom( env.getProperty( "support.email" ) );
+        return email;
+
+    }
+}
