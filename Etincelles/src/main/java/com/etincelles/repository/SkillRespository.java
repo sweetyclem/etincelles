d4267b604b72e082cfc7587336ab6970d93c23c7
sweetyclem <pirlot.clementine@gmail.com>
2017-05-21 11:09:43 +0200

    Add list of all existing skills in directory filter select

--- Etincelles/src/main/java/com/etincelles/repository/SkillRespository.java
+++ Etincelles/src/main/java/com/etincelles/repository/SkillRespository.java
@@ -1,9 +1,13 @@
 package com.etincelles.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.etincelles.entities.Skill;
 
 public interface SkillRespository extends CrudRepository<Skill, Long> {
     Skill findByname( String name );
+
+    List<Skill> findAll();
 }
